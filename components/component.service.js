4360398d971382feffcaa5162ba2ca0bea1e2905
georgiosan <georgiosanagnostou@hotmail.com>
2021-09-22 23:04:51 +0300

    Code cleanup and minor updates to component constructor arguments

--- components/component.service.js
+++ components/component.service.js
@@ -1,14 +1,22 @@
 import { create } from "../load/layout.js";
 
 export default class Component {
+  // setting id, category & identifier as private because they should be non-enumerable
   #id;
+  #category;
+  #identifier;
   static count = 0;
-  constructor(name, description, price) {
+  constructor(name, price, description, identifier, category) {
     this.#id = Component.count++;
-    this.img = `img-${this.id}`;
+    this.img = "";
     this.name = name;
     this.description = description;
     this.price = price;
+    this.#category = category.toLowerCase();
+    this.#identifier = identifier;
+  }
+  static resetCount() {
+    Component.count = 0;
   }
   get keys() {
     return Object.keys(this);
@@ -16,20 +24,32 @@ export default class Component {
   get id() {
     return this.#id;
   }
+  get category() {
+    return this.#category;
+  }
+  get identifier() {
+    return this.#identifier;
+  }
   render() {
     const main = document.querySelector(".main");
-    const parentElement = create("div", `menu-item${this.id}`, "menu-item");
+    const parentElement = create(
+      "div",
+      `menu-item-${this.id}`,
+      "menu-item",
+      `${this.category}-${this.identifier}`,
+      `${this.category}`
+    );
     // creates wrapper divs and div content and appends to main
     this.keys.forEach((key) => {
-      let tag = "p";
+      let tag = "span";
       let wrapper = create("div", `${key}-wrapper`, "wrapper");
       if (key === "img") tag = "img";
-      if (key === "name") tag = "h1";
+      if (key === "name") tag = "h2";
+      if (key === "description") tag = "p";
+      // TODO need to split this to add populateImages in there
       let content = wrapper.appendChild(create(`${tag}`, `${key}-${tag}`));
       parentElement.appendChild(content);
     });
     main.appendChild(parentElement);
   }
-  //  TODO create the populate method for the actual content
-  populate() {}
 }
