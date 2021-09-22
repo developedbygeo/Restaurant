9cbeba6488384651182c73b7db2dbb317eb1452e
georgiosan <georgiosanagnostou@hotmail.com>
2021-09-22 22:59:13 +0300

    Adding index.js. The current structure is working as intended

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import init from "../load/init";
-import layout from "../load/layout.js";
+import headerLayout from "../load/layout.js";
+import { addContent } from "../load/layout.js";
+import menuHandler from "/structure/body.structure.js";
+import Component from "/components/component.service.js";
+import data from "/menu-data.json";
 
 init();
-layout();
+headerLayout();
 
-console.log("It is working");
+const { starters, light, grilled, wine } = data.menu;
+starters.forEach((starter) => {
+  const { name, price, description, id } = starter;
+  const newMenuItem = new Component(name, price, description, id, "starters");
+  newMenuItem.render();
+});
+addContent("starters");
