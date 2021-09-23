fa008c7989e4d82e5b2651ea60eafbbd844b8336
georgiosan <georgiosanagnostou@hotmail.com>
2021-09-24 02:00:45 +0300

    Updated index.js according to the other changes that were made

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import init from "../load/init";
+import { init, initializeContent } from "../load/init";
 import headerLayout from "../load/layout.js";
-import { addContent } from "../load/layout.js";
-import menuHandler from "/structure/body.structure.js";
-import Component from "/components/component.service.js";
-import data from "/menu-data.json";
+import { enableButtons } from "/structure/menu.change.js";
+import enableContactButton from "/structure/contact.handle.js";
 
 init();
 headerLayout();
-
-const { starters, light, grilled, wine } = data.menu;
-starters.forEach((starter) => {
-  const { name, price, description, id } = starter;
-  const newMenuItem = new Component(name, price, description, id, "starters");
-  newMenuItem.render();
-});
-addContent("starters");
+enableButtons();
+initializeContent();
+enableContactButton();
