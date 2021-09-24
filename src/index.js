cd8fea7228e3702610f5c5e98951f0a41f304308
georgiosan <georgiosanagnostou@hotmail.com>
2021-09-24 21:44:54 +0300

    Adjusted the imports and function invocations accordingly

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { init, initializeContent } from "../load/init";
 import headerLayout from "../load/layout.js";
+import { createFooter } from "../load/layout.js";
 import { enableButtons } from "/structure/menu.change.js";
-import enableContactButton from "/structure/contact.handle.js";
+import { enableLogoClick } from "/structure/menu.change.js";
+import "../styles/style.scss";
 
 init();
 headerLayout();
+createFooter();
 enableButtons();
+enableLogoClick();
 initializeContent();
-enableContactButton();
