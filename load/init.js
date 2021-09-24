ac5235a88aa8c27124dfca2fd81ad7bb0d6de308
georgiosan <georgiosanagnostou@hotmail.com>
2021-09-24 21:43:12 +0300

    Added the createFooter function

--- load/init.js
+++ load/init.js
@@ -1,7 +1,7 @@
-import "../styles/style.scss";
 import data from "/menu-data.json";
 import Component from "/components/component.service.js";
 import { addContent } from "../load/layout.js";
+import { resetDOM } from "../structure/menu.change";
 
 function initPage() {
   const main = document.querySelector("main");
@@ -9,12 +9,16 @@ function initPage() {
 }
 
 function initializeContent() {
+  const startersBtn = document.querySelector(".button0");
+  resetDOM();
+  Component.resetCount();
   const { starters } = data.menu;
   starters.forEach((starter) => {
     const { name, price, description, id } = starter;
     const newMenuItem = new Component(name, price, description, id, "starters");
     newMenuItem.render();
   });
+  startersBtn.classList.add("button-active");
   addContent("starters");
 }
 export { initPage as init, initializeContent };
