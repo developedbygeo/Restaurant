015c8a4c2d749303bfffe65432cf34ad23ce7734
georgiosan <georgiosanagnostou@hotmail.com>
2021-09-24 21:47:17 +0300

    Added enableLogoClick, adjusted the buttonClassHandler and correctMainClass to change layout

--- structure/menu.change.js
+++ structure/menu.change.js
@@ -1,18 +1,27 @@
 import Component from "/components/component.service.js";
 import data from "/menu-data.json";
 import { addContent } from "../load/layout";
+import { initializeContent } from "../load/init";
+import { createContactPage } from "./contact.handle";
 
-// TODO add the .active class on the clicked button
-
+// enables the navigation buttons for both menu and contact
 function enableButtons() {
-  const allButtons = document.querySelectorAll(".button");
+  const allButtons = document.querySelectorAll(".btn");
   allButtons.forEach((button) => {
-    button.addEventListener("click", changeMenu);
+    button.addEventListener("click", (e) => {
+      buttonClassHandler(e);
+      if (button.textContent === "Contact") {
+        createContactPage();
+      } else {
+        correctMainClass();
+        changeMenu(e);
+      }
+    });
   });
 }
-// TODO assign .main class because contact.handle will change the class
-function changeMenu() {
-  const selection = this.textContent.toLowerCase();
+// clears the DOM, creates new objects and renders them
+function changeMenu(e) {
+  const selection = e.target.textContent.toLowerCase();
   const selectedObject = data.menu[`${selection}`];
   Component.resetCount();
   resetDOM();
@@ -29,10 +38,41 @@ function changeMenu() {
   });
   addContent(`${selection}`);
 }
+// removes every main child
 function resetDOM() {
   const main = document.querySelector(".main");
   while (main.hasChildNodes()) {
     main.removeChild(main.lastChild);
   }
 }
-export { enableButtons, changeMenu, resetDOM };
+// logo click generates the starters menu
+function enableLogoClick() {
+  const logo = document.querySelector(".logo");
+  logo.addEventListener("click", initializeContent);
+}
+// handles the class toggling for menu buttons
+function buttonClassHandler(e) {
+  const allButtons = document.querySelectorAll(".btn");
+  const currentClass = e.target.classList[1];
+  e.target.classList.add(`${currentClass}-active`);
+  allButtons.forEach((button) => {
+    if (button != e.target && e.target.textContent !== "Contact") {
+      button.classList.remove("contact-active");
+      button.classList.remove(`${currentClass}-active`);
+    }
+    if (e.target.textContent === "Contact") {
+      button.classList.remove("button-active");
+    }
+  });
+}
+function correctMainClass() {
+  const main = document.querySelector(".main");
+  main.classList.remove("main-contact");
+}
+export {
+  enableButtons,
+  changeMenu,
+  resetDOM,
+  enableLogoClick,
+  buttonClassHandler,
+};
