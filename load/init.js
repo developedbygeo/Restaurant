f0e21765c35ea17b516a9ae923c326d29d40954f
georgiosan <georgiosanagnostou@hotmail.com>
2021-09-25 20:29:44 +0300

    Logo-clicking bug fixed, now the button-active class is removed from the previously selected

--- load/init.js
+++ load/init.js
@@ -1,7 +1,7 @@
 import data from "/menu-data.json";
 import Component from "/components/component.service.js";
 import { addContent } from "../load/layout.js";
-import { resetDOM } from "../structure/menu.change";
+import { resetDOM, correctMainClass } from "../structure/menu.change";
 
 function initPage() {
   const main = document.querySelector("main");
@@ -9,7 +9,10 @@ function initPage() {
 }
 function initializeContent() {
   const startersBtn = document.querySelector(".button0");
+  const allBtns = document.querySelectorAll(".btn");
+  const ctcBtn = document.querySelector(".contact");
   resetDOM();
+  correctMainClass();
   Component.resetCount();
   const { starters } = data.menu;
   starters.forEach((starter) => {
@@ -17,6 +20,9 @@ function initializeContent() {
     const newMenuItem = new Component(name, price, description, id, "starters");
     newMenuItem.render();
   });
+  allBtns.forEach((button) => button.classList.remove("button-active"));
+  ctcBtn.classList.remove("contact-active");
+  console.log(allBtns);
   startersBtn.classList.add("button-active");
   addContent("starters");
 }
