612b5d48503490d10a60e6566cc159b6cef385a1
georgiosan <georgiosanagnostou@hotmail.com>
2021-09-28 02:52:04 +0300

    Added copyWebpackPlugin settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
   devtool: "source-map",
   plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./src/img/grilled", to: "./grilled" },
+        { from: "./src/img/light", to: "./light" },
+        { from: "./src/img/map", to: "./map" },
+        { from: "./src/img/starters", to: "./starters" },
+        { from: "./src/img/wines", to: "./wines" },
+      ],
+    }),
     new HtmlWebpackPlugin({
       title: "Restaurant Page",
       favicon: "./src/favicon/favicon-96x96.png",
@@ -17,7 +27,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    assetModuleFilename: "img/[name]-[hash:3].[ext][query]",
+    assetModuleFilename: "assets/[name]-[hash:3].[ext][query]",
   },
   module: {
     rules: [
